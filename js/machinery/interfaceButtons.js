978d48c03315969ed382ae22dbf36c0c12eddac3
Eugene Rumynskiy <erumynskiy@gmail.com>
2017-07-07 15:34:18 +0300

    -input and view buttons are done
    Signed-off-by: Eugene Rumynskiy <erumynskiy@gmail.com>

--- js/machinery/interfaceButtons.js
+++ js/machinery/interfaceButtons.js
@@ -1,90 +1,126 @@
 var motifPickerButtons = (function () {
     var _fileName = "motifPickerButtons",
-        _currentClass,
-        _nextClass;
 
+        _currentState,
+        _nextState;
 
 
     var create = function () {
-        _currentClass = "flattened";
-        _nextClass = initClasses(["hidden", "full-screen", "flattened"]);
+        _currentState = {};
+        _nextState = {};
+
+        _nextState["textarea"] = getStatesObject(["sequence-input", "title-input"]);
+        _nextState["inputView"] = getStatesObject(["hidden", "flattened", "full-screen"]);
 
         setButtons();
     };
 
 
-    var initClasses = function (classesToSet) {
-        var nextClass = {};
+    var getStatesObject = function (states) {
+        var statesObject = {};
 
-        if ((classesToSet == undefined) || (classesToSet.length < 2)) {
-            errorHandler.logError({"fileName": _fileName, "message": "initClasses are undefined"});
+        if ((states === undefined) || (states.length < 2)) {
+            errorHandler.logError({"fileName": _fileName, "message": "getStatesObject are undefined"});
         } else {
-            for (var i = 0; i < classesToSet.length - 1; i++) {
-                nextClass[classesToSet[i]] = classesToSet[i + 1];
+            for (var i = 0; i < states.length - 1; i++) {
+                statesObject[states[i]] = states[i + 1];
             }
 
-            i = classesToSet.length - 1;
-            nextClass[classesToSet[i]] = classesToSet[0];
+            i = states.length - 1;
+            statesObject[states[i]] = states[0];
         }
 
-        return nextClass;
+        return statesObject;
     };
 
 
-    var getCurrentClass = function () {
-        return _currentClass;
+    var getCurrentState = function (target) {
+        return _currentState[target];
     };
 
 
-    var setCurrentClassTo = function (newCurrentClass) {
-        _currentClass = newCurrentClass;
+    var setCurrentStateTo = function (newState, target) {
+        _currentState[target] = newState;
     };
 
 
-    var getNextClass = function (currentClass) {
-        if (!(currentClass in _nextClass)) {
-            errorHandler.logError({"fileName": _fileName, "message": "getNextClass, currentClass not in _nextClass"});
+    var getNextState = function (currentState, target) {
+        if (!(currentState in _nextState[target])) {
+            errorHandler.logError({"fileName": _fileName, "message": "getNextState, currentState not in _nextState"});
         }
-        return _nextClass[currentClass];
+        return _nextState[target][currentState];
     };
 
 
     var setButtons = function () {
-        setViewButton();
-        setTitleButton();
+        setSwitchViewButton();
+        setSwitchInputButton();
     };
 
 
-    var setViewButton = function () {
-        var $button = $("#change-sequence-view");
+    var setSwitchViewButton = function () {
+        var $button = $("#change-sequence-view"),
+            currentState = "flattened";
+
+        setCurrentStateTo(currentState, "inputView");
+        $button.children(".icon").html("View: " + currentState);
+
         $button.on('click', function(event) {
             event.preventDefault();
-
-            var $source = $(this),
-                $target = $("#" + $source.attr("data-apply-to-id")),
-                currentClass = getCurrentClass(),
-                nextClass = getNextClass(currentClass);
-
-            $target.removeClass(currentClass);
-            $target.addClass(nextClass);
-            setCurrentClassTo(nextClass);
+            switchViewToNextState($(this));
         });
     };
 
 
-    var setTitleButton = function () {
-        var $button = $("#show-title-button");
+    var switchViewToNextState = function ($button) {
+        var currentInputState = getCurrentState("textarea"),
+            $target = $("#" + currentInputState),
+
+            currentViewState = getCurrentState("inputView"),
+            nextState = getNextState(currentViewState, "inputView");
+
+        $target.removeClass(currentViewState);
+        $target.addClass(nextState);
+
+        setCurrentStateTo(nextState, "inputView");
+        $button.children(".icon").html("View: " + nextState);
+    };
+
+
+    var setSwitchInputButton = function () {
+        var $button = $("#show-title-button"),
+            currentState = "sequence-input";
+
+        setCurrentStateTo(currentState, "textarea");
+        $("#title-input").addClass("hidden");
+        $button.children(".icon").html("Input: " + currentState);
+
+
         $button.on('click', function(event) {
             event.preventDefault();
-
-            $("#sequence-input").toggleClass("hidden");
-            $("#title-input").toggleClass("hidden");
+            switchInputToNextState($(this));
         });
+    };
+
+
+    var switchInputToNextState = function ($button) {
+        var currentInputState = getCurrentState("textarea"),
+            $target = $("#" + currentInputState);
+        $target.addClass("hidden");
+
+        var nextInputState = getNextState(currentInputState, "textarea");
+        $target = $("#" + nextInputState);
+        $target.removeClass("hidden flattened full-screen");
+        $target.addClass(getCurrentState("inputView"));
+
+        setCurrentStateTo(nextInputState, "textarea");
+
+        $button.children(".icon").html("Input: " + nextInputState);
     };
 
 
     return {
-        create: create,
+        create: create
     };
 }());
 /**
